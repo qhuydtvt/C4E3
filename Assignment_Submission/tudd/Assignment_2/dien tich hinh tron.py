ebc42dad77e50c5ef56751c864a1703a96b584c3
Laptop of Dinh Tu <Laptop of Dinh Tu>
2016-03-03 00:38:17 +0700

    homework

---
+++ Assignment_Submission/tudd/Assignment_2/dien tich hinh tron.py
@@ -0,0 +1,5 @@
+while True:
+    r = int(input('Radius?',))
+    pi = 3.14 
+    A = (r**2)*3.14
+    print("Area=",A)
---
+++ Assignment_Submission/tudd/Assignment_2/doi tu C sang F.py
@@ -0,0 +1,4 @@
+while True:
+    c = int(input('enter the temperature in Celsius?'))
+    f = (c*(1.8))+ 32 
+    print(str.format( "{0}(C)={1}",c,f))
