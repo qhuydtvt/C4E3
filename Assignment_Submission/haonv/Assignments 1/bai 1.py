f6a09d31e222e41de90691f94862067d056bc13d
nguyenhaoC4e <nguyenhao120691@gmail.com>
2016-03-02 23:46:16 +0700

    dang bai tap

---
+++ Assignment_Submission/haonv/Assignments 1/bai 1.py
@@ -0,0 +1,4 @@
+## bai 1
+## 1.1 de kiem tra loai cua mot bien ta su dung " type "de kiem tra
+## 1.2 loi cu phap la : khi su dung cac ky tu dac biet, su dung so trong bien, khi trung voi keyworks's
+## 1.2.1 3 vi du la : 123, @$% , us
---
+++ Assignment_Submission/haonv/Assignments 1/bai 4.py
@@ -0,0 +1,9 @@
+##bai 4 viet 1 chuong trinh yeu cau nguoi dung nhap vao hai so la :
+## so luong vi khuan ban dau va khoang thoi gian.
+##tinh toan va in ra tong so vi khuan B
+
+a = int(input("nhap vao so vi khuan"))
+b = int(input("nhap vao so phut"))
+c = a*b
+print("tong so vi khuan la : ",c)
+
