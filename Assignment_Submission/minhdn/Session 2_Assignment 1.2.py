424260a08605c1b4f943fc7ec5c4faea413aa3e3
Dao Nhat Minh <nhatmjnh7294@gmail.com>
2016-03-03 21:47:43 +0700

    minh nop bai

---
+++ Assignment_Submission/minhdn/Session 2_Assignment 1.2.py
@@ -0,0 +1,4 @@
+r = int(input ("Radius? "))
+pi = 3.141592
+a = r*r*pi
+print ("Area =",a)
---
+++ Assignment_Submission/minhdn/Session 2_Assignment 1.3.py
@@ -0,0 +1,3 @@
+c = int(input("Enter the tempurature in Celsius? "))
+f = c*1.8 + 32
+print (str.format("{0}(C) = {1} (F)", c, f))
