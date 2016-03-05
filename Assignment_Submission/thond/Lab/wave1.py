585c28b3080378cea2c15487f89e53fb9498271d
James Nguyen <James Nguyen>
2016-03-05 22:29:24 +0700

    lab

--- Assignment_Submission/thond/Lab/wave1.py
+++ Assignment_Submission/thond/Lab/wave1.py
@@ -1,24 +1,4 @@
 from turtle import *
-##color('green', 'yellow')
-##begin_fill()
-##for i in range(6):
-##    circle(100)
-##    right(60)
-##end_fill()
-##done()
-##
-##
-##i = 0
-##n = 10
-##g = 360/n
-##while i < (g): 
-##    circle (50)
-##    right(n)
-##    speed (0)
-##    i = i +1 
-##end_fill()
-
-##forward (50)
 
 fd (50)
 right (90)
