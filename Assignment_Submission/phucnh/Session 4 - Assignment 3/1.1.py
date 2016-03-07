c6fce40591f633a41b940117d9b41faa2946a62b
nhphuc <phuc1009@gmail.com>
2016-03-07 11:42:14 +0700

    lab 1 + asignment 4.3 thiếu bài 1.3, 1.4

---
+++ Assignment_Submission/phucnh/Session 4 - Assignment 3/1.1.py
@@ -0,0 +1,21 @@
+from turtle import *
+color('red', 'white')
+begin_fill()
+i=0
+left(30)
+while i<4:
+    
+    forward(120)
+    right(60)
+    forward(120)
+    right(120)
+    forward(120)
+    right(60)
+    forward(120)
+    right(30)
+    i=i+1
+
+##if abs(pos()) < 1:
+    
+end_fill()
+done()
