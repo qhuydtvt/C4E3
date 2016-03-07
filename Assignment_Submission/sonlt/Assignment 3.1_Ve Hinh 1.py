5b25dfa360ed4c02d827f21f1eb8533c8f81eb4a
sonlt <lethanhson1989@gmail.com>
2016-03-07 22:29:35 +0700

    May bai sau kho qua, anh ko di hoc buoi Lab nen chua nghi ra. Co gi goi y anh nhe. Cam on em

---
+++ Assignment_Submission/sonlt/Assignment 3.1_Ve Hinh 1.py
@@ -0,0 +1,22 @@
+from turtle import *
+color('red')
+i=0
+begin_fill
+right(120)
+bk(100)
+left(60)
+bk(100)
+left(120)
+bk(100)
+left(60)
+bk(100)   
+while i<3:
+    left(30)
+    bk(100)
+    left(60)
+    bk(100)
+    left(120)
+    bk(100)
+    left(60)
+    bk(100)
+    i=i+1
---
+++ Assignment_Submission/sonlt/Assignment 3.1_Ve Hinh 2.py
@@ -0,0 +1,7 @@
+from turtle import *
+color('green')
+i=0
+begin_fill
+while i<10:
+    circle(10+i*10)
+    i=i+1
