3bbbdd9a7eaad0b6171e1c8cecd7518be711f21c
sonlt <lethanhson1989@gmail.com>
2016-03-11 22:03:45 +0700

    no message

---
+++ Assignment_Submission/sonlt/Assignment 4_List&Range.py
@@ -0,0 +1,44 @@
+##color_list=["Blue","Red","Black","Pink","Brown","Yellow"]
+##print(str.format('My color list is \n{0}',color_list))
+
+##print('color list at index 3:',color_list[3])
+
+##i=int(input('Enter a number from 0-5: '))
+##print('your color: ',color_list[i])
+
+##a=0
+##length=len(color_list)
+##while a<length:
+##    print(color_list[a])
+##    a=a+1
+
+##color_list=['Blue','Red','Black','Pink','Brown','Yellow']
+##i=[0,1,2,3,4,5]
+##b=input('What is your favorite color? ')
+##if b==color_list:
+##    print(str.format('My index is {0}',
+    
+##my_sheep_size=[5,7,300,90,24,50,75]
+##print('Hello, my name is Hiep and these are my sheep size',my_sheep_size)
+
+###my_sheep_size=[5,7,300,90,24,50,75]
+##a=max(my_sheep_size)
+##b=my_sheep_size.pop(2)
+##print('Hello, my name is Hiep and these are my sheep size',my_sheep_size)
+##print(str.format('Now my biggest sheep size is {0}. We can shear',a))
+##print(str.format('After shearing, we have [{0}] flocks',my_sheep_size))
+
+##i=0
+##def d(i):
+##    m=['Mot','Hai','Ba','Tu']
+##def Thang(i): 
+##    c=[5+50*i,7+50*i,300+50*i,90+50*i,24+50*i,50+50*i,75+50*i]
+##    a=max(c)
+##    b=c.pop(2)
+##    print('Hello, my name is Hiep and these are my sheep size',c)
+##    print(str.format('Now my biggest sheep size is {0}. We can shear',a))
+##    print(str.format('After shearing, we have [{0}] flocks',c))
+##while i<5:
+##    i=i+1
+##    print(str.format('Thang {0}',d(i)))
+##    print(Thang(i))
