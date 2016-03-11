171d71706b8c1319d3f770a87c13f7b52569a094
Dinh Van Quy <dvquy.13@gmail.com>
2016-03-11 19:18:41 +0700

    Assignment 2.6 ngày 10/03/2016

---
+++ Assignment_Submission/quydv/Assignment 2.1/Assignment 2.6/Ex 3. Extract even numbers.py
@@ -0,0 +1,4 @@
+input_list = input("Enter a list of number using space as delimeter: ")
+list_of_num = [int(x) for x in input_list.split()]
+list_of_even = [x for x in list_of_num if x % 2 == 0]
+print(list_of_even)
