21fd804bb0d595d5b71aba31c067517da9d7ec48
nhphuc <phuc1009@gmail.com>
2016-03-16 16:12:14 +0700

    them phần sửa và xóa phim

--- Assignment_Submission/phucnh/Session 7 mongodb/mongodb.py
+++ Assignment_Submission/phucnh/Session 7 mongodb/mongodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId
+
 
 # Connect to Mlab
 URI ="mongodb://c4e3:CodeTheChange@ds015879.mlab.com:15879/c4e3_movie_db"
@@ -12,23 +13,70 @@ movies = db["movies"]
 def duyet_phim():
     i=0
     for movie in movies.find():
-        i=i+1   
-        print(i,movie['trans_name'],"(",movie['year'],")")
-        print(movie['org_name'],"-" , movie['quality'])
+        i=i+1
+        #print(movie)
+        print(i,movie['trans_name'],"(",movie['year'],")")
+        print(movie['org_name'],"-" , movie['quality'])
         print();
 
-duyet_phim()
+def them_phim():
+        org_name=input("Ten tieng Anh cua phim: ")
+        trans_name=input("Ten tieng Viet cua phim: ")
+        quality=input("Chat luong phim? HD/Cam: ")
+        year=int(input("Nam san xuat: "))
+        movies.insert_one({
+            "org_name" : org_name,
+            "trans_name": trans_name,
+            "quality": quality,
+            "year": year})
+def xoa_phim():
+    phim_xoa=int(input("Ban muon xoa phim stt so may: "))
+    i=0
+    for movie in movies.find():
+        i=i+1        
+        if phim_xoa==i:
+           movies.delete_one({"_id":movie['_id']}) 
 
-them_phim=input("Ban co muon them phim? Co/Khong:")
-if them_phim == "co":
-    org_name=input("Ten tieng Anh cua phim: ")
-    trans_name=input("Ten tieng Viet cua phim: ")
-    quality=input("Chat luong phim? HD/Cam: ")
-    year=input("Nam san xuat: ")
-    movies.insert_one({
-        "org_name" : org_name,
-        "trans_name": trans_name,
-        "quality": quality,
-        "year": year})
+
+
+def sua_phim():
+    phim_sua=int(input("Ban muon sua phim stt so may: "))
+    i=0
+    for movie in movies.find():
+        i=i+1        
+        if phim_sua==i:
+            muc_sua=input("Ban muon sua phan nao: Ten TV/Ten TA/CL/Nam: ")
+            muc_sua=muc_sua.lower()
+            if muc_sua=="ten tv":
+                trans_name=input("Ten tieng Viet moi cua phim: ")
+                movies.update_one({
+                    "_id":movie['_id']},
+                    {'$set':{"trans_name": trans_name}})
+            elif muc_sua=="ten ta":
+                org_name=input("Ten tieng Anh moi cua phim: ")
+                movies.update_one({
+                    "_id":movie['_id']},
+                    {'$set':{"trans_name": trans_name}})    
+            elif muc_sua=="cl":
+                quality=input("Chat luong moi cua phim: ")
+                movies.update_one({
+                    "_id":movie['_id']},
+                    {'$set':{"quality": quality}})
+            elif muc_sua=="nam":
+                year=int(input("Nam sx moi cua phim: "))
+                movies.update_one({
+                    '_id':movie['_id']},
+                    {'$set':{'year': year}})           
+           
+       
+duyet_phim()
+thao_tac=input("Ban muon them phim, sua phim hay xoa phim? Them/Sua/Xoa: ");
+thao_tac=thao_tac.lower()
+if thao_tac=="them":
+    them_phim()
+elif thao_tac=="xoa":
+    xoa_phim()
+elif thao_tac=="sua":
+    sua_phim()
 
 duyet_phim()
