613a13fc8b6bad46b52b600bb509d8b748182fe1
James Nguyen <James Nguyen>
2016-03-24 16:49:23 +0700

    Assgn 9

---
+++ Assignment_Submission/thond/Assgn 9/scafef2.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+file = open ("scafef.html", "rb") #r = read b = binary
+web = file.read ()
+web_string = web.decode("utf-8") #decode TV
+
+soup = BeautifulSoup(web_string, "html.parser")
+
+taco = soup.find("table", id = "tableContent")
+tab = taco.find("tbody")
+tr = tab.find_all("tr")
+for i in tr:
+    for td in i.find_all("td", "b_r_c"):
+        t = td.get_text()
+        if len(t) != 0:
+            print(t, end = "|")
