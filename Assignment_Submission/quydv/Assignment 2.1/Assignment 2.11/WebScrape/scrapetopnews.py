2a4b322b594a11e9212c8572d05acfc2661da1c8
Dinh Van Quy <dvquy.13@gmail.com>
2016-03-26 01:33:28 +0700

    Assignment 2.11 ngày 24/03/2016

--- Assignment_Submission/quydv/Assignment 2.1/Assignment 2.11/WebScrape/scrapetopnews.py
+++ Assignment_Submission/quydv/Assignment 2.1/Assignment 2.11/WebScrape/scrapetopnews.py
@@ -1,22 +1,31 @@
 from bs4 import BeautifulSoup
+import urllib.request
+import xlwt
 
-file = open("VNExpress.htm", "rb")
-vnexpress = file.read()
-vnexpress_string = vnexpress.decode("utf-8")
+vnexpress_url = "http://vnexpress.net"
 
-soup = BeautifulSoup(vnexpress_string, "html.parser")
+web_page = urllib.request.urlopen(vnexpress_url)
+html = web_page.read()
+html_string = html.decode("utf-8")
+
+soup = BeautifulSoup(html_string, "html.parser")
 
 hot_news_divs = soup.find_all("div", "box_sub_hot_news", "scroll-pane")
 
 for div in hot_news_divs:
     all_a = div.find_all("a")
-    i = 1
-    for a in all_a:
-        print(str(i) + ".", a.contents[0])
-        i += 1
+    for n in range(0, len(all_a) - 3):
+        print(str(n+1) + ".", all_a[n].contents[0])
 
+wb = xlwt.Workbook()
+ws = wb.add_sheet("VNExpress_hot_news")
 
+for hot_news in hot_news_divs:
+    for i in range(len(all_a) - 3):
+        ws.write(i, 0, str(i+1) + "." + hot_news.find_all("a")[i].contents[0])
+    # r = 0
+    # for a in hot_news.find_all("a"):
+    #     ws.write(r, 0, a.contents[0])
+    #     r += 1
 
-
-
-
+wb.save("VNExpress.xls")
