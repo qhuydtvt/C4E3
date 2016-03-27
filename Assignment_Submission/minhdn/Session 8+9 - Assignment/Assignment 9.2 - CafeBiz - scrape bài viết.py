f046161215de01890d1383e534d26c2822333374
Dao Nhat Minh <nhatmjnh7294@gmail.com>
2016-03-28 01:41:59 +0700

    Thắc mắc dòng "Số lượng: 5 người" và dòng có chữ đỏ

---
+++ Assignment_Submission/minhdn/Session 8+9 - Assignment/Assignment 9.2 - CafeBiz - scrape bài viết.py
@@ -0,0 +1,35 @@
+__author__ = 'MS MINH'
+
+from bs4 import BeautifulSoup
+file = open("cafebiz tuyen cay viet.html", "rb")
+web = file.read()
+web_content = web.decode("utf-8")
+soup = BeautifulSoup(web_content, "html.parser")
+
+cont = soup.find("div", class_= "content")
+title = cont.find("h1").string
+# print(title)
+
+h2 = cont.find("h2").string
+# print(h2)
+
+image = cont.find("img")
+# print(image)
+
+detail_content = cont.find("div", class_="detail-content")
+ps = detail_content.find_all("p")
+
+for p in ps:
+    try:
+        if p.string == None:
+            strong = p.find("strong").string
+            span = p.find("span").string
+            print(strong, span)
+        else:
+            print(p.string)
+    except AttributeError:
+        continue
+
+
+
+
