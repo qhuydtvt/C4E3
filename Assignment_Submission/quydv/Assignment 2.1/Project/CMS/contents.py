118fe28546bb929d4965e17deec3fe83f5364bb4
Dinh Van Quy <dvquy.13@gmail.com>
2016-04-10 16:33:17 +0700

    update

--- Assignment_Submission/quydv/Assignment 2.1/Project/CMS/contents.py
+++
@@ -1,5 +0,0 @@
-def Content():
-    menu = [{"name": "Home", "href": "home", "content": "Home"},
-            {"name": "Lecture1", "href": "lecture-1", "content": "ABC"},
-            {"name": "Lecture2", "href": "lecture-2", "content": "DEF"}]
-    return menu
---
+++ Assignment_Submission/quydv/Assignment 2.1/Project/c4e3 cms/c4e3 cms.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+from mongoengine import *
+from flask import *
+from werkzeug.utils import *
+
+
+#Upload data to Mongo
+connect('nim', host='mongodb://hiep:hiepxanh@ds015929.mlab.com:15929/nim')
+
+class lecture(Document):
+    href = StringField()
+    title = StringField()
+    brief = ListField(StringField())
+    document = ListField(DictField())
+    assignment = ListField(DictField())
+
+lecture_list = lecture.objects
+print(lecture_list)
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def loginpage():
+#     return render_template('login.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request == "POST":
+        u=request.form['username']
+        p=request.form['password']
+        if u=="1" and p=="1":
+            return render_template("Index.html",
+                                   lecture_list = lecture_list)
+    return render_template('login.html')
+
+
+@app.route('/<lectureHref>')
+def lectureHref(lectureHref):
+    for lecture in lecture_list:
+        if lecture.href == lectureHref:
+            return render_template("lecture.html",
+                                   lecture = lecture,
+                                   lecture_list = lecture_list)
+
+#Lay anh tai local host:
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+@app.route('/index')
+def index():
+    return render_template("Index.html",
+                           lecture_list = lecture_list)
+
+
+if __name__ == '__main__':
+    app.run()
