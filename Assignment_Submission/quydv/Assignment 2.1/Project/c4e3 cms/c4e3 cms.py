e6cf035beb7e162b20145386882a84bc9aa02aad
Dinh Van Quy <dvquy.13@gmail.com>
2016-04-12 19:01:30 +0700

    Update 3 html

--- Assignment_Submission/quydv/Assignment 2.1/Project/c4e3 cms/c4e3 cms.py
+++ Assignment_Submission/quydv/Assignment 2.1/Project/c4e3 cms/c4e3 cms.py
@@ -15,18 +15,44 @@ class lecture(Document):
 
 lecture_list = lecture.objects
 
+class Users(Document):
+    username = StringField()
+    password = StringField()
+
+# user1 = Users(username = "dvquy",
+#               password = "dvquy123")
+# user2 = Users(username = "ntnhan",
+#               password = "ntnhan123")
+# user2.save()
+# user1.save()
+
+user_list = Users.objects
+print(user_list)
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        u=request.form['username']
-        p=request.form['password']
-        if u=="1" and p=="1":
-            return render_template("Index.html",
-                                   lecture_list = lecture_list)
-    return render_template('login.html')
+    error = ""
+    try:
+        if request.method == "POST":
+            u = request.form['username']
+            p = request.form['password']
+            for user in user_list:
+                if user.username == u and user.password == p:
+                    # session["logged_in"] = True
+                    # session["username"] = request.form("username")
+                    return redirect(url_for("index"))
+                    # return render_template("Index.html",
+                                           # username = user.username,
+                    #                        lecture_list = lecture_list)
+            error = "Invalid credentials. Please try again!"
+
+    except Exception as e:
+        flash(e)
+        return render_template("login.html", error = error)
+
+    return render_template('login.html', error = error)
 
 @app.route('/<lectureHref>/')
 def lectureHref(lectureHref):
@@ -35,7 +61,6 @@ def lectureHref(lectureHref):
             return render_template("lecture.html",
                                    lecture = lecture,
                                    lecture_list = lecture_list)
-
 #Lay anh tai local host:
 @app.route('/<path:path>/')
 def static_file(path):
@@ -45,6 +70,12 @@ def static_file(path):
 def index():
     return render_template("Index.html",
                            lecture_list = lecture_list)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
 
 
 if __name__ == '__main__':
